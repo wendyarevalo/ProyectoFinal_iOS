550b02ce1711ec3c3f34df1682f02dad90c6c3aa
Mac1 <mac1@imac-de-mac1.local>
2018-12-04 00:45:40 +0800

    adding a function

--- ProyectoFinal/ViewController.swift
+++ ProyectoFinal/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func aDocmicilio(_ sender: UISwitch) {
+        if(sender.isOn){
+            
+        }else{
+            
+        }
+    }
+    
 }
 
