ec3603fe5db7008c6de64855fb8c16441e7d9e46
Mac1 <mac1@imac-de-mac1.local>
2018-12-06 01:37:12 +0800

    adding table view controller este es el comite hue hue

--- ProyectoFinal/ViewController.swift
+++ ProyectoFinal/ViewController.swift
@@ -10,17 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var search: UITextField!
+    @IBOutlet weak var fieldSearch: UITextField!
     
-    @IBOutlet weak var location: UITextField!
+    @IBOutlet weak var fieldLocation: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
     @IBAction func buscar(_ sender: UIButton) {
-        let con = LugarConexion(search: search.text ?? "torta", location: location.text ?? "Mexico", withDelivery: true)
+        let con = LugarConexion(search: fieldSearch.text ?? "torta", location: fieldLocation.text ?? "Mexico", withDelivery: true)
         con.toDo()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Se obtiene la busqueda
+        guard let search = fieldSearch.text else { return }
+        guard let location = fieldLocation.text else { return }
+        
+        // Se busca que sea la transicion correcta
+        if segue.identifier == "searchSegue" {
+            // Se crea la instancia
+            let lugaresController = segue.destination as! LugaresTableViewController
+            
+            lugaresController.search = search
+            lugaresController.location =  location
+            
+        }
     }
     
 }
